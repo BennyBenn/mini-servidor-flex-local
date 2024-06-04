927d594ac870c8a47541dfe4bd43213240581dbb
BennyBenn <rhadonizer@gmail.com>
2024-06-04 00:18:21 -0600

    no se porque no jala

--- login.js
+++ login.js
@@ -1,10 +1,60 @@
-const usuarios=["Obama@usa.gov"];
-const contras=["BinLaden"];
+let nombr=document.getElementById('nombre').value;
+let contra=document.getElementById('password').value;
 
-function entrada(usr,pass){
+const usuarios=["1234"];
+const contras=["1234"];
 
-    if(usuarios.contains(usr) && contras.contains(pass) ){
+function entrada() {
+    validadorDD();
+}
+
+
+function validadorDD(){
+  if(usuarios.includes(nombr) && contras.includes(contra))
+    if(usuarios.indexOf(nombr) === contras.indexOf(contra))
+        window.location.href = "inicio.html";
+
+}
+
+
+/*
+function entrada(){    
+    let usrIndex = recUsr();
+    let pwdIndex = recPwd();
+
+    if (usrIndex > -1 && pwdIndex > -1 && usrIndex === pwdIndex)
         window.location.replace("inicio.html");
 
+    ////
+    let nombr = document.getElementById('nombre').value;
+    let contra = document.getElementById('password').value;
+  
+    if (recUsr(nombr) > -1 && recPwd(contra) > -1) {
+      window.location.href("inicio.html");
+      console.log("entra");
     }
-}
+    console.log("no entra ");
+        
+}*/
+
+
+
+/*
+function recUsr(usr) {
+    usuarios.forEach((user, index) => {
+      if (user === usr) {
+        return index;
+      }
+    });
+    return -1;
+  }
+  
+  function recPwd(pass) {
+    contras.forEach((pwd, index) => {
+      if (pwd === pass) {
+        return index;
+      }
+    });
+    return -1;
+  }
+  */
